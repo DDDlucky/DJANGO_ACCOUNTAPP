0c4d06b0d1937f187259840ccbc3fdaa2a1434ec
DDDlucky <d.ono2105@gmail.com>
2021-06-23 14:33:42 +0900

    deploy

--- techpit/wsgi.py
+++ techpit/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for techpit project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techpit.settings')
 
 application = get_wsgi_application()
+
+# ========以下をすべて追加========
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
